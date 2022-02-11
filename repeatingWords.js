1fe1dbfbdb3d05a1a9c6700e0ec3e08cddf23744
shwethaa-rajesh <shwethaa_r@mckinsey.com>
2022-02-11 13:57:57 +0530

    fix: modularised repeatingWords.js

--- repeatingWords.js
+++ repeatingWords.js
@@ -1,16 +1,25 @@
 let sentence = "Hi hello Hi hello";
-function findDuplicateWords(sentence) {
-   let words = sentence.split(" ");
-   let res = [];
+function getWords(sentence)
+{
+    return sentence.split(" ");
+}
+function getDuplicateWords(words)
+{
+   let duplicates = [];
    for(let i = 0; i < words.length; i++){
       if(words.indexOf(words[i]) !== words.lastIndexOf(words[i])){
-         if(!res.includes(words[i])){
-            res.push(words[i]);
+         if(!duplicates.includes(words[i])){
+            duplicates.push(words[i]);
          }
       }
    }
-   if(res.length===0)
-        return "No duplicates found"
-   return res.join(" , ").concat(" is repeated");
+   return duplicates
 }
-console.log(findDuplicateWords(sentence));
+function ifDuplicateWords(sentence) {
+   let words = getWords(sentence)
+   duplicates=getDuplicateWords(words)
+   if(duplicates.length===0)
+        return "No duplicates found"
+   return duplicates.join(" , ").concat(" is repeated");
+}
+console.log(ifDuplicateWords(sentence));
